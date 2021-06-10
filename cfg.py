b36d333371e64c876110c2b2b337999735249471
leander <leander97@gmx.de>
2021-06-10 16:55:41 -0400

    added the DataModes class (training, validation, testing)

--- cfg.py
+++ cfg.py
@@ -14,7 +14,16 @@ SIM_FILES = dict(DM = '/tigress/lthiele/Illustris_300-1_Dark/simulation.hdf5',
 
 # how many particles of each type to use per halo
 # set to None if all particles are to be used
+# if an integer larger than one, not the fraction but this number is used
 PRT_FRACTION = dict(DM = None, TNG = None)
 
 # whether to divide the coordinates by R200c
 NORMALIZE_COORDS = True
+
+# fraction of samples to use for each mode -- training is inferred
+SAMPLE_FRACTIONS = dict(validation = 0.2,
+                        testing = 0.1)
+
+# seed to shuffle the samples -- keep constant to get consistent splittings
+SAMPLE_SHUFFLE_SEED = 137
+
---
+++ data_modes.py
@@ -0,0 +1,45 @@
+from enum import Enum, auto
+
+import numpy as np
+
+import cfg
+
+class DataModes(Enum) :
+    """
+    the three possible modes for which we may want to load data
+    """
+    TRAINING = auto()
+    VALIDATION = auto()
+    TESTING = auto()
+
+
+    def __repr__(self) :
+        """
+        a nice human readable representation
+        """
+    #{{{
+        if self is DataModes.TRAINING :
+            return 'training'
+        elif self is DataModes.VALIDATION :
+            return 'validation'
+        elif self is DataModes.TESTING :
+            return 'testing'
+    #}}}
+
+
+    def sample_indices(self) :
+        """
+        returns the indices in the halo catalog that are used for this data mode
+        """
+    #{{{
+        with np.load(cfg.HALO_CATALOG) as f :
+            Nsamples_tot = len(f['idx_DM'])
+        
+        try :
+            Nsamples = int(round(Nsamples_tot * cfg.SAMPLE_FRACTIONS[str(self)]))
+        except KeyError : # we have hit the one that is not specified
+            Nsamples = Nsamples_tot - sum(int(round(Nsamples_tot * v)) for v in cfg.SAMPLE_FRACTIONS.values())
+
+        return np.random.default_rng(cfg.SAMPLE_SHUFFLE_SEED).choice(np.arange(Nsamples_tot),
+                                                                     size=Nsamples, replace=False)
+    #}}}
