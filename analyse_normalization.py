9a73247daceb25a5e0a1a1cb90bdbaf8768285f4
Leander Thiele <lthiele@princeton.edu>
2021-08-04 18:34:37 -0400

    wrote a bit in

--- analyse_normalization.py
+++ analyse_normalization.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-nets = ['encoder', 'local', 'deformer', 'vae', ]
+nets = ['encoder', 'local', 'vae', ]
+# deformer has nans, deal with this later
 
 scalars = dict()
 desc = dict()
@@ -11,4 +12,19 @@ with np.load('normalization_data.npz') as f :
         scalars[s] = f['scalars_%s'%s]
         desc[s] = str(f['desc_%s'%s])
 
-print(max(a.shape[-1] for a in scalars.keys()))
+for s in nets :
+    
+    fig, ax = plt.subplots(nrows=9, ncols=5, figsize=(20,20))
+    ax = ax.flatten()
+
+    a = scalars[s]
+    for ii in range(a.shape[-1]) :
+        ax[ii].hist(a[:,ii], bins=20)
+        ax[ii].text(0.05, 0.05, str(ii), transform=ax[ii].transAxes)
+        avg = np.mean(a[:,ii])
+        std = np.std(a[:,ii])
+        ax[ii].text(0.05, 0.95, 'mu=%f s=%f'%(avg,std), transform=ax[ii].transAxes)
+    
+    fig.suptitle(desc[s])
+    
+    fig.savefig('normalization_hists_%s.pdf'%s, bbox_inches='tight')
