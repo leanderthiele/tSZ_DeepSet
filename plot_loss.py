9cd0c2a19b521c2e41540ff75b2e5b9769614eca
leander <leander97@gmx.de>
2021-08-07 16:07:01 -0400

    flexible plot range in ul panel

--- plot_loss.py
+++ plot_loss.py
@@ -57,8 +57,10 @@ ax_mean_kld = ax[1][1]
 ax_epoch_loss.scatter(tg, t, label='training', s=3+20*(tlogm-vmin)/(vmax-vmin))
 ax_epoch_loss.scatter(vg, v, label='validation', s=3+20*(vlogm-vmin)/(vmax-vmin))
 ax_epoch_loss.plot([0,1e3], [0,1e3], linestyle='dashed', color='black')
-ax_epoch_loss.set_xlim(1e-3,1e0)
-ax_epoch_loss.set_ylim(1e-3,1e0)
+min_lim = min((ax_epoch_loss.get_xlim()[0], ax_epoch_loss.get_ylim()[0]))
+max_lim = min((ax_epoch_loss.get_xlim()[1], ax_epoch_loss.get_ylim()[1]))
+ax_epoch_loss.set_xlim(min_lim, max_lim)
+ax_epoch_loss.set_ylim(min_lim, max_lim)
 ax_epoch_loss.set_yscale('log')
 ax_epoch_loss.set_xscale('log')
 
