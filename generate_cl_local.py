83d4c563a768cd089c8b88ac31a80beee1747993
leander <leander97@gmx.de>
2021-08-31 21:34:15 -0400

    added generate_cl_local.py, maybe too many degrees of freedom

---
+++ generate_cl_local.py
@@ -0,0 +1,31 @@
+
+def generate_cl(trial) :
+
+    out = []
+    out.append('NET_ARCH=dict(origin=False,batt12=False,deformer=False,encoder=False,'\
+                             'scalarencoder=False,decoder=True,vae=False,local=True)')
+    out.append('LOCAL_NLATENT=1')
+    out.append('OUTPUT_NFEATURES=1')
+
+    lr = trial.suggest_float('lr', 1e-4, 1e-1, log=True)
+    out.append('ONE_CYCLE_LR_KWARGS["local"]["max_lr"]=%.8e'%lr)
+
+    N_local = trial.suggest_int('N_local', 32, 1024, log=True)
+    out.append('N_LOCAL=%d'%N_local)
+
+    R_local = trial.suggest_float('R_local', 50.0, 200.0)
+    out.append('R_LOCAL=%.8e'%R_local)
+
+    N_layers = trial.suggest_categorical('N_layers', (0,1))
+    out.append('ORIGIN_DEFAULT_NLAYERS=%d'%N_layers)
+
+    MLP_N_layers = trial.suggest_int('MLP_N_layers', 2, 8)
+    out.append('LOCAL_MLP_NLAYERS=%d'%MLP_N_layers)
+
+    MLP_N_hidden = trial.suggest_int('MLP_N_hidden', 32, 256)
+    out.append('LOCAL_MLP_NHIDDEN=%d', MLP_N_hidden)
+
+    pass_N = trial.suggest_categorical('pass_N', ('True', 'False'))
+    out.append('LOCAL_PASS_N=%s'%pass_N)
+
+    return out
