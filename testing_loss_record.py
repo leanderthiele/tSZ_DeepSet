4d19fb85249442d4dab3cea2109546781a029adf
leander <leander97@gmx.de>
2021-09-09 15:49:25 -0400

    fixed typo

--- testing_loss_record.py
+++ testing_loss_record.py
@@ -32,7 +32,7 @@ class TestingLossRecord :
         np.savez(os.path.join(cfg.RESULTS_PATH, 'loss_testing_%s.npz'%cfg.ID),
                  loss=np.array(self.loss_arr),
                  KLD=np.array(self.KLD_arr),
-                 guess_loss=np.array(self.KLD_arr),
+                 guess_loss=np.array(self.guess_loss_arr),
                  logM=np.array(self.logM_arr),
                  idx=np.array(self.idx_arr, dtype=int))
     #}}}
