7d3c9b7df194fa524389ea3e8b9ef7d77f27db79
leander <leander97@gmx.de>
2021-09-10 14:53:05 -0400

    show all projections in plot_prediction.py

--- plot_prediction.py
+++ plot_prediction.py
@@ -2,7 +2,6 @@
 Command line arguments:
     [1] ID
     [2] halo idx
-    [3] axis over which to sum
 """
 
 from sys import argv
@@ -16,14 +15,14 @@ import cfg
 
 ID = argv[1].replace('testing_', '').replace('.sbatch', '')
 halo_idx = int(argv[2])
-axis = int(argv[3])
 
 path = os.path.join(cfg.RESULTS_PATH, 'prediction_%s_idx%d.bin'%(ID, halo_idx))
 
 box = CubifyPrediction(path)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(ncols=3)
 
-ax.matshow(np.sum(box, axis=axis))
+for ii in range(3) :
+    ax[ii].matshow(np.sum(box, axis=ii))
 
 plt.show()
