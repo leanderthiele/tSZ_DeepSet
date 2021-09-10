6fd4601ae7a51d1da88789fe27a63ec893b3f66f
leander <leander97@gmx.de>
2021-09-10 15:01:21 -0400

    added target to plot_prediction.py

--- plot_prediction.py
+++ plot_prediction.py
@@ -16,13 +16,18 @@ import cfg
 ID = argv[1].replace('testing_', '').replace('.sbatch', '')
 halo_idx = int(argv[2])
 
-path = os.path.join(cfg.RESULTS_PATH, 'prediction_%s_idx%d.bin'%(ID, halo_idx))
+path_target = cfg._STORAGE_FILES['TNG']%(halo_idx, 'box_%d_cube_Pth'%cfg.TNG_RESOLUTION)
+path_prediction = os.path.join(cfg.RESULTS_PATH, 'prediction_%s_idx%d.bin'%(ID, halo_idx))
 
-box = CubifyPrediction(path)
+box_target = np.fromfile(path_target, dtype=np.float32).reshape([cfg.TNG_RESOLUTION,]*3)
+box_prediction = CubifyPrediction(path_prediction)
 
-fig, ax = plt.subplots(ncols=3)
+fig, ax = plt.subplots(ncols=3, nrows=2)
+ax_target = ax[0]
+ax_prediction = ax[1]
 
 for ii in range(3) :
-    ax[ii].matshow(np.sum(box, axis=ii))
+    ax_target[ii].matshow(np.sum(box_target, axis=ii))    
+    ax_prediction[ii].matshow(np.sum(box_prediction, axis=ii))
 
 plt.show()
