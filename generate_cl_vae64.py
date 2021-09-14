8ee4bdae043508313d5c4445518fdaa03a173adf
leander <leander97@gmx.de>
2021-09-13 20:41:46 -0400

    removed most of the decoder settings from generate_cl_vae64.py to focus on the VAE things for now

--- generate_cl_vae64.py
+++ generate_cl_vae64.py
@@ -11,9 +11,7 @@ def generate_cl(trial) :
     out.append('DECODER_DEFAULT_GLOBALS_PASSED=True')
     out.append('DECODER_DEFAULT_BASIS_PASSED=False')
 
-# FIXME
-# We have removed this for debugging purposes
-#    out.append('VALIDATION_EPOCHS=10')
+    out.append('VALIDATION_EPOCHS=10')
     out.append('N_LOCAL["validation"]=512')
     out.append('PRT_FRACTION["DM"]["validation"]=2048')
     out.append('PRT_FRACTION["TNG"]["validation"]=4096')
@@ -34,35 +32,28 @@ def generate_cl(trial) :
     out.append('ORIGIN_DEFAULT_NHIDDEN=226')
     out.append('BASIS_NOISE=None')
     out.append('PRT_FRACTION["DM"]["training"]=2048')
-    out.append('N_LOCAL["training"]=310')
+    out.append('N_LOCAL["training"]=256')
     out.append('R_LOCAL=310.0')
+
+    # things we can play with later but want to fix at the moment to focus on the VAE stuff
+    out.append('DECODER_DEFAULT_NLAYERS=3')
+    out.append('DECODER_DEFAULT_NHIDDEN=196')
+    out.append('DECODER_DROPOUT=0.1')
+    out.append('DECODER_VISIBLE_DROPOUT=0.1')
+    out.append('GLOBALS_NOISE=30.0')
+    out.append('WEIGHT_DECAY["decoder"]=1e-5')
 
     out.append('NET_ID=("optuna_localorigin64_nr561","local","origin","batt12")')
     out.append('NET_FREEZE=["local","origin","batt12"]')
 
-    lr_decoder = trial.suggest_float('lr_decoder', 1e-6, 1e-3, log=True)
+    lr_decoder = trial.suggest_float('lr_decoder', 1e-6, 1e-1, log=True)
     out.append('ONE_CYCLE_LR_KWARGS["decoder"]["max_lr"]=%.8e'%lr_decoder)
 
     lr_vae = trial.suggest_float('lr_vae', 1e-7, 1e-1, log=True)
     out.append('ONE_CYCLE_LR_KWARGS["vae"]["max_lr"]=%.8e'%lr_vae)
 
-    wd_decoder = trial.suggest_float('wd_decoder', 1e-8, 1e-1, log=True)
-    out.append('WEIGHT_DECAY["decoder"]=%.8e'%wd_decoder)
-
     wd_vae = trial.suggest_float('wd_vae', 1e-8, 1e-1, log=True)
     out.append('WEIGHT_DECAY["vae"]=%.8e'%wd_vae)
-
-    N_layers_decoder = trial.suggest_int('N_layers_decoder', 2, 5)
-    out.append('DECODER_DEFAULT_NLAYERS=%d'%N_layers_decoder)
-
-    N_hidden_decoder = trial.suggest_int('N_hidden_decoder', 120, 256)
-    out.append('DECODER_DEFAULT_NHIDDEN=%d'%N_hidden_decoder)
-
-    dropout_decoder = trial.suggest_float('dropout_decoder', 0.0, 0.2)
-    out.append('DECODER_DROPOUT=%.8e'%dropout_decoder)
-
-    visible_dropout_decoder = trial.suggest_float('visible_dropout_decoder', 0.0, 0.3)
-    out.append('DECODER_VISIBLE_DROPOUT=%.8e'%visible_dropout_decoder)
 
     N_layers_vae = trial.suggest_int('N_layers_vae', 2, 5)
     out.append('VAE_NLAYERS=%d'%N_layers_vae)
@@ -73,13 +64,20 @@ def generate_cl(trial) :
     residuals_noise = trial.suggest_float('residuals_noise', 1e-2, 1e1, log=True)
     out.append('RESIDUALS_NOISE=%.8e'%residuals_noise)
 
-    globals_noise = trial.suggest_float('globals_noise', 15, 40)
-    out.append('GLOBALS_NOISE=%.8e'%globals_noise)
-
-    kld_scaling = trial.suggest_float('kld_scaling', 1e-3, 1e2, log=True)
+    kld_scaling = trial.suggest_float('kld_scaling', 1e-5, 1e0, log=True)
     out.append('KLD_SCALING=%.8e'%kld_scaling)
+
+    kld_annealing_start = trial.suggest_float('kld_annealing_start', 0.0, 0.3)
+    out.append('KLD_ANNEALING_START=%.8e'%kld_annealing_start)
     
-    kld_annealing_end = trial.suggest_float('kld_annealing_end', 0.2, 1.0)
+    kld_annealing_end = trial.suggest_float('kld_annealing_end', 0.3, 1.0)
     out.append('KLD_ANNEALING_END=%.8e'%kld_annealing_end)
+
+    gradient_clip = trial.suggest_float('gradient_clip', 1e-1, 1e1)
+    out.append('GRADIENT_CLIP=%.8e'%gradient_clip)
+
+    N_latent_vae = trial.suggest_categorical('N_latent_vae', (1, 2))
+    out.append('VAE_NLATENT=%d'%N_latent_vae)
+
 
     return out
