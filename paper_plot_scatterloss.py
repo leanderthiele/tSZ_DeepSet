be7a5c6f25aa39c5acb973f01a94c746e93476cb
Leander Thiele <lthiele@princeton.edu>
2021-09-15 23:06:42 -0400

    paper_plot_scatterloss.py sort of working

--- paper_plot_scatterloss.py
+++ paper_plot_scatterloss.py
@@ -13,7 +13,12 @@ IDs = {'origin64': ('Origin+GNFW', 'blue'),
 
 MARKER_SCALE = 1.0
 
+EXCESS_SPACE = 0.1
+
 fig, ax = plt.subplots()
+ax_linear = ax.twiny()
+
+guess_loss = None
 
 for ID, (label, color) in IDs.items() :
     
@@ -33,20 +38,26 @@ for ID, (label, color) in IDs.items() :
         else :
             gauss_loss_all = None
 
-    idx = np.unique(idx_all)
+    idx = np.sort(np.unique(idx_all))
     loss = np.empty(len(idx))
-    guess_loss = np.empty(len(idx))
-    gauss_loss = np.empty((len(idx),gauss_loss_all.shape[1])) if gauss_loss_all is not None else None
+    guess_loss_here = np.zeros(len(idx))
+    gauss_loss = np.empty((len(idx), gauss_loss_all.shape[1])) if gauss_loss_all is not None else None
     logM = np.empty(len(idx))
+
     for ii, this_idx in enumerate(idx) :
         mask = this_idx == idx_all 
         # loss is MSE (no sqrt!), so this makes sense
         loss[ii] = np.sum(N_TNG_all[mask] * loss_all[mask]) / np.sum(N_TNG_all[mask])
-        guess_loss[ii] = np.sum(N_TNG_all[mask] * guess_loss_all[mask]) / np.sum(N_TNG_all[mask])
+        guess_loss_here[ii] = np.sum(N_TNG_all[mask] * guess_loss_all[mask]) / np.sum(N_TNG_all[mask])
         if gauss_loss is not None :
             gauss_loss[ii] = np.sum(N_TNG_all[mask][:,None] * gauss_loss_all[mask], axis=0) / np.sum(N_TNG_all[mask])
         logM[ii] = logM_all[mask][0]
         assert all(abs(logM[ii]/logm-1) < 1e-5 for logm in logM_all[mask])
+
+    if guess_loss is None :
+        guess_loss = guess_loss_here
+    else :
+        assert np.allclose(guess_loss, guess_loss_here)
 
     vmin = 8.518
     vmax = 11.534
@@ -54,21 +65,38 @@ for ID, (label, color) in IDs.items() :
                s=MARKER_SCALE*(3+20*(logM-vmin)/(vmax-vmin)), c=color)
 
     if gauss_loss is not None :
-        parts = ax.violinplot(gauss_loss, positions=guess_loss, showmeans=True)
+        lg = np.log(guess_loss)
+        lg_min = np.min(lg)
+        lg_max = np.max(lg)
+        # plot this on the [0, 1] x-scale
+        parts = ax_linear.violinplot(gauss_loss.T, positions=(lg-lg_min)/(lg_max-lg_min),
+                                     showmeans=False, showextrema=False, widths=0.02)
         for pc in parts['bodies'] :
             pc.set_facecolor(color)
-
-ax.set_yscale('log')
-ax.set_xscale('log')
 
 ax.set_xlabel('GNFW loss')
 ax.set_ylabel('network loss')
 
-min_lim = 0.9*min((ax.get_xlim()[0], ax.get_ylim()[0]))
-max_lim = 1.1*max((ax.get_xlim()[1], ax.get_ylim()[1]))
+ax.set_yscale('log')
+ax.set_xscale('log')
+
+min_lim = (1-EXCESS_SPACE)*min((ax.get_xlim()[0], ax.get_ylim()[0]))
+max_lim = (1+EXCESS_SPACE)*max((ax.get_xlim()[1], ax.get_ylim()[1]))
 ax.plot([min_lim, max_lim], [min_lim, max_lim], linestyle='dashed', color='black')
 ax.set_xlim(min_lim, max_lim)
 ax.set_ylim(min_lim, max_lim)
+
+# now we need to figure out how to adjust limits on our fake axis with linear scale
+lg = np.log(guess_loss)
+lg_min = np.min(lg)
+lg_max = np.max(lg)
+l_min = np.log(ax.get_xlim()[0])
+l_max = np.log(ax.get_xlim()[1])
+add_max = (l_max - lg_max) / (lg_max - lg_min)
+add_min = (l_min - lg_min) / (lg_max - lg_min)
+ax_linear.set_xlim(add_min, 1+add_max)
+
+ax_linear.set_xticks([])
 
 ax.legend(loc='upper left', frameon=False)
 
