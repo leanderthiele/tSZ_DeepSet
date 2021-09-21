b00c77238a592929333b67718466118d1092626b
leander <leander97@gmx.de>
2021-09-20 22:17:59 -0400

    small tweaks to get_compute_time.py

--- get_compute_time.py
+++ get_compute_time.py
@@ -15,14 +15,20 @@ total_time = None
 for fname in fnames :
     study = optuna.load_study(study_name=fname[:-3], storage='sqlite:///%s'%fname)
     d = study.trials_dataframe()
-    delta_t = ( d['datetime_complete']-d['datetime_start'] ).sum()
+
+    try :
+        delta_t = ( d['datetime_complete']-d['datetime_start'] ).sum()
+    except KeyError :
+        print('%s not yet populated'%fname)
+        continue
     
     if total_time is None :
         total_time = delta_t
     else :
         total_time += delta_t
 
-    print('%s for %s'%(str(delta_t.round('s')), fname))
+    print('%s :'%fname)
+    print('\t%s'%str(delta_t.round('s')))
 
 print('-------------------------------------')
 print('%s total'%str(total_time.round('s')))
